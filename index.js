827776e0d431776002810c27f2702ca5d8f5723c
Vasyl Zeikan <vasazejkan5@gmail.com>
2024-10-31 21:19:11 +0000

    feat: add node-naviagtor dependency, get method on /api/whoami path

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+const { Navigator } = require('node-navigator');
+
+const navigator = new Navigator();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -22,6 +25,14 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: navigator.language,
+    software: navigator.userAgent
+  }); 
 });
 
 // listen for requests :)
